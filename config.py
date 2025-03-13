374464c9985712a0e31afe22613dda12e590f492
HR Ranger <hrranger555@gmail.com>
2025-03-13 10:37:47 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+# Set Google Dialogflow Authentication
+GOOGLE_APPLICATION_CREDENTIALS = "service-account.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GOOGLE_APPLICATION_CREDENTIALS
